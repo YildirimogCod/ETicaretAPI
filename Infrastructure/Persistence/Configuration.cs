c6ade3e9d4a3ee320c422c20ed12f06f12dd0b75
yildirimogcod <yildirimogcod@gmail.com>
2025-05-07 18:44:06 +0300

    Add project files.

---
+++ Infrastructure/Persistence/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ETicaretAPI.Persistence
+{
+    static class Configuration
+    {
+        static public string ConnectionString
+        {
+            get
+            {
+                ConfigurationManager configuration = new ConfigurationManager();
+                configuration.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(),"../../Presentation/ETicaretAPI.API"));
+                configuration.AddJsonFile("appsettings.json");
+
+                return configuration.GetConnectionString("PostgreSql");
+
+            }
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Contexts/ETicaretDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ETicaretAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETicaretAPI.Persistence.Contexts
+{
+    public class ETicaretDbContext : DbContext
+    {
+        public ETicaretDbContext(DbContextOptions options) : base(options)
+        {}
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
